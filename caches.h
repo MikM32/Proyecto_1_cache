0a0ba22e531cfdef9e6c2383388163c6ecc76cc8
Miguel Matute <miguelmatute2004@gmail.com>
2024-06-10 20:30:03 -0400

    Arreglos

--- caches.h
+++ caches.h
@@ -7,25 +7,28 @@
 #include <cmath>
 #include <algorithm>
 #include <list>
+#include "list.hpp"
 
 using namespace std;
 
-void cache_comp_directa(int numBloques, int tamBloque, string fdirec_ruta);
-void cache_asoc_conjuntos(int numBloques, int tamBloques, int tamConjuntos, string fdirec_ruta);
-void cache_asociativa_compl(int numBloques, int tamBloques, string fdirec_ruta);
+typedef unsigned int uint32;
 
 class CampoCache
 {
     private:
         bool validez = false;
+        int dato;
         int etiqueta = 0;
         int tiempo = -1;
     public:
 
+        CampoCache()
+        {
 
+        }
         //getters
 
-        
+
         int getTiempo()
         {
             return this->tiempo;
@@ -40,8 +43,17 @@ class CampoCache
             return this->etiqueta;
         }
 
+        int getDato()
+        {
+            return this->dato;
+        }
+
         //setters
-        
+
+        void setDato(int d)
+        {
+            this->dato = d;
+        }
         void setTiempo(int t)
         {
             this->tiempo = t;
@@ -54,13 +66,231 @@ class CampoCache
         {
             this->etiqueta = et;
         }
-		
+
 		void cambiar(bool val, int et, int tmp)
 		{
 			this->setValidez(val);
 			this->setEtiqueta(et);
 			this->setTiempo(tmp);
 		}
+};
+
+class LineaCache
+{
+    public:
+
+        bool validez=false;
+        uint32 etiqueta;
+
+        bool operator==(LineaCache& l2)
+        {
+            return (this->etiqueta == l2.etiqueta) && (this->validez == l2.validez);
+        }
+};
+
+class Cache
+{
+    protected:
+        int nBloques;
+        int tamBloques;
+        int contador_tiempo=0;
+
+    public:
+
+        int getBloquesNum()
+        {
+            return this->nBloques;
+        }
+
+        int getTamBloques()
+        {
+            return this->tamBloques;
+        }
+
+        virtual bool acceso(int direccion)
+        {
+            cout << "No hace nada"<<endl;
+            return false;
+        }
+
+};
+
+class CacheDirecta : Cache
+{
+    private:
+        vector<CampoCache> cache;
+
+    public:
+
+        CacheDirecta()
+        {
+
+        }
+
+        CacheDirecta(int nBloques, int tamBloques)
+        {
+            this->nBloques = nBloques;
+            this->tamBloques = tamBloques;
+            this->cache = vector<CampoCache>(nBloques, CampoCache());
+        }
+
+        bool acceso(int direccion)
+        {
+            int etiqueta;
+            int indice;
+            int despBloque = log2(this->tamBloques);
+
+            etiqueta = direccion >> despBloque;
+            indice = etiqueta % this->nBloques;
+            if(cache[indice].getEtiqueta() == etiqueta && cache[indice].getValidez()) //Si el bit de validez es verdadero y las etiquetas coinciden
+            {
+
+                return true;
+            }
+            else
+            {
+                cache[indice].setEtiqueta(etiqueta);
+                cache[indice].setValidez(true);     // Pone a verdadero el bit de validez
+
+            }
+            return false;
+        }
+};
+
+
+class CacheConjuntos : Cache
+{
+    private:
+        List<List<LineaCache>*> cache;
+        int nVias;
+        int tamConjuntos;
+
+    public:
+
+        CacheConjuntos()
+        {
+
+        }
+
+        CacheConjuntos(int numBloques, int tamBloques, int tamConjuntos)
+        {
+            this->nVias = numBloques / tamConjuntos;
+            this->nBloques = numBloques;
+            this->tamBloques = tamBloques;
+            this->tamConjuntos = tamConjuntos;
+            for(int i=0; i< nVias; i++)
+            {
+                cache.insertAtLast(new List<LineaCache>());
+            }
+        }
+
+        bool acceso(int direccion)
+        {
+            int despBloque = log2(this->tamBloques);
+            int etiqueta = direccion << despBloque;
+            int indiceConjunto = etiqueta % this->nVias;
+            bool flag_acierto = false;
+
+            LineaCache linea;
+            linea.etiqueta=etiqueta;
+            linea.validez=true;
+
+            int ind = this->cache.getValueAtIndex(indiceConjunto)->search(linea);
+            if(ind < 0)
+            {
+                if(!this->cache.getValueAtIndex(indiceConjunto)->isEmpty())
+                {
+                    this->cache.getValueAtIndex(indiceConjunto)->removeAtFirst();
+                }
+
+                this->cache.getValueAtIndex(indiceConjunto)->insertAtLast(linea);
+            }
+            else
+            {
+                flag_acierto=true;
+            }
+
+            return flag_acierto;
+
+
+        }
+
+};
+
+class CacheCompAsoc : Cache
+{
+    private:
+        List<LineaCache> cache;
+        LineaCache buffer; // Para el prefetch
+        int nVias;
+        int curVias=1;
+
+    public:
+
+        CacheCompAsoc()
+        {
+
+        }
+
+        CacheCompAsoc(int nVias, int tamBloques)
+        {
+            this->nVias = nVias;
+            this->tamBloques = tamBloques;
+        }
+
+        bool acceso(uint32 direccion)
+        {
+            int despBloque = log2(this->tamBloques);
+            uint32 etiqueta = direccion >> despBloque;
+            bool flag_acierto = false;
+
+            LineaCache linea;
+            linea.etiqueta=etiqueta;
+            linea.validez=true;
+
+            if(linea == this->buffer)
+            {
+                flag_acierto=true;
+            }
+            else
+            {
+                int ind = this->cache.search(linea);
+                if(ind < 0)
+                {
+                    if(this->curVias == this->nVias)
+                    {
+                        this->cache.removeAtFirst();
+                        this->cache.insertAtLast(linea);
+                    }
+                    else
+                    {
+                        this->curVias++;
+                        this->cache.insertAtLast(linea);
+                    }
+
+
+                }
+                else
+                {
+                    flag_acierto=true;
+                }
+            }
+
+
+
+            return flag_acierto;
+        }
+
+        void prefetch(int direccion)
+        {
+            int despBloque = log2(this->tamBloques);
+            int etiqueta = direccion >> despBloque;
+
+            this->buffer.etiqueta = etiqueta;
+            this->buffer.validez = true;
+        }
+
+
 };
 
 #endif // CACHES_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 
 #define NUM_VIAS 2
 
+
 using namespace std;
 
 /*
@@ -23,6 +24,106 @@ using namespace std;
     Descripcion:    Simular los tres tipos de cache en c++. recibe como entrada un archivo con una cantidad determinada de direcciones
                     y como salida crea un archivo donde sea vea cada acierto y fallo para cada direccion.
 */
+
+
+void cache_comp_directa(int numBloques, int tamBloque, string fdirec_ruta)
+{
+    CacheDirecta cache(numBloques, tamBloque);
+
+
+    ofstream salida;
+    ifstream fdirec;
+    uint32 direccion;
+
+    salida.open("salida_composicion_directa.txt");
+    fdirec.open(fdirec_ruta);
+
+    while(fdirec >> direccion)
+    {
+        if(cache.acceso(direccion))
+        {
+            salida << "Direccion: "<< direccion << "| Acierto"<<endl;
+        }
+        else
+        {
+            salida << "Direccion: "<< direccion << "| Fallo"<<endl;
+        }
+    }
+
+    salida.close();
+    fdirec.close();
+}
+
+int ultimo_usado(vector<int> tiempos)
+{
+    int indice;
+    int minimo = 1000;
+    for(size_t i=0; i<tiempos.size(); i++)
+    {
+        if(tiempos[i] < minimo)
+        {
+            indice = i;
+            minimo = tiempos[i];
+        }
+    }
+
+    return indice;
+}
+
+void cache_asoc_conjuntos(int numBloques, int tamBloques, int tamConjuntos, string fdirec_ruta)
+{
+
+    CacheConjuntos cache(numBloques, tamBloques, tamConjuntos);
+    uint32 direccion;
+
+    ofstream salida;
+    ifstream fdirec;
+    salida.open("salida_asociativa_cojuntos.txt");
+    fdirec.open(fdirec_ruta);
+
+    while(fdirec >> direccion)
+    {
+        if(cache.acceso(direccion))
+        {
+            salida << "Direccion: "<< direccion << "| Acierto"<<endl;
+        }
+        else
+        {
+            salida << "Direccion: "<< direccion << "| Fallo"<<endl;
+        }
+    }
+
+    salida.close();
+    fdirec.close();
+}
+
+//no terminado
+void cache_asociativa_compl(int numBloques, int tamBloques, string fdirec_ruta)
+{
+
+    CacheCompAsoc cache(numBloques, tamBloques);
+    uint32 direccion;
+
+    ofstream salida;
+    ifstream fdirec;
+    salida.open("salida_compl_asociativa.txt");
+    fdirec.open(fdirec_ruta);
+
+    while(fdirec >> direccion)
+    {
+        if(cache.acceso(direccion))
+        {
+            salida << "Direccion: "<< direccion << "| Acierto"<<endl;
+        }
+        else
+        {
+            salida << "Direccion: "<< direccion << "| Fallo"<<endl;
+        }
+    }
+    fdirec.close();
+    salida.close();
+
+}
 
 int main(int argc, const char* argv[])
 {
@@ -38,7 +139,7 @@ int main(int argc, const char* argv[])
 
         cache_comp_directa(NUM_BLOQUES, BLOQUE_SIZE, fdirec_ruta);
         cache_asoc_conjuntos(NUM_BLOQUES, BLOQUE_SIZE, NUM_VIAS, fdirec_ruta);
-		cache_asociativa_compl(NUM_BLOQUES, BLOQUE_SIZE, fdirec_ruta);
+        cache_asociativa_compl(NUM_BLOQUES, BLOQUE_SIZE, fdirec_ruta);
 
 
     }
